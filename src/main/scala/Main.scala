5579871559ab88d400519833424066e5abebb1f1
Ostabo <osi.borkenhagen@gmail.com>
2021-10-28 22:50:02 +0200

    first matrix impl.

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,35 +1,59 @@
 package scala
 
 @main def Hex: Unit = {
-  val h = new HexField(1, 1);
+  val h = new HexField(0, 0);
+  //h.placeX(0, 0)
+  //h.placeX(0, 1)
   print(h.field);
 }
 
 case class HexField(val lines:Int = 6, val col:Int = 9):
-  //val matrix = Array.ofDim[Char](lines, col)
   var condition = 1;
   if (col == 0 || lines == 0) {
     condition = 0;
   }
+  
   val usecol = col / 2
+  val matrix = Array.ofDim[Char](lines + 1, usecol + 1)
+  val stone = ' ';
+  if (condition != 0) {
+    
+    // Initialisation
+    for(i <- 0 until lines) {
+      for(j <- 0 until usecol) {
+        matrix(i)(j) = stone;
+      }
+    }
+  } else {
+    matrix(0)(0) = stone
+  }
+  
   val eol = "\n" * condition
-  //var stone = ' ';
-  def top = "/   \\" * condition + "___/   \\" * usecol + eol
+  
+  //def top = "/   \\" * condition + "___/   \\" * usecol + eol
   def bot = "\\___/" * condition + "   \\___/" * usecol + eol
   def edgetop = " ___ " * condition + "    ___ " * usecol + eol
+  
 
-  //def placeX(x:Int, y:Int) = {
-  //  matrix(x)(y) = 'X'
-  //}
-  //def placeO(x:Int, y:Int) = {
-  //  matrix(x)(y) = 'O'
-  //}
+  def placeX(x:Int, y:Int) = {
+    matrix(x)(y) = 'X'
+  }
+  def placeO(x:Int, y:Int) = {
+    matrix(x)(y) = 'O'
+  }
 
-  //def top(line:Int): String = {
-  //  var res = "/ " + matrix(line)(0) + " \\" * condition
-  //  for (x <- (1 to col)) {
-  //    res += "___/ " + matrix(line)(x) + " \\" * usecol + eol
-  //  }
-  //  res
-  //}
-  def field = eol + edgetop + (top + bot) * lines
+  def top(line:Int): String = {
+    
+    var res = "/ " * condition + matrix(line)(0).toString * condition + " \\" * condition
+    for (x <- 0 until usecol) {
+      res += "___/ " + matrix(line)(x) + " \\" * usecol
+    }
+    res + "\n" * condition
+  }
+  def field = {
+    var res = eol + edgetop
+    for(l <- 0 until lines) {
+      res += (top(l) + bot)
+    }
+    res
+  }
--- src/test/scala/HexSpecTest.scala
+++ src/test/scala/HexSpecTest.scala
@@ -15,7 +15,7 @@ class HexSpecTest extends AnyWordSpec {
                 hex.edgetop should be(new HexField(1, hex.col).edgetop);
             }
             "have lines" in {
-                "\n" + hex.edgetop + hex.top + hex.bot should be(new HexField(1, hex.col).field)
+                "\n" + hex.edgetop + hex.top(0) + hex.bot should be(new HexField(1, hex.col).field)
             }
             "be equal to a field the same size" in {
                 hex.field should be(new HexField(hex.lines, hex.col).field)
@@ -30,7 +30,7 @@ class HexSpecTest extends AnyWordSpec {
                 hex.edgetop should be(new HexField(1, hex.col).edgetop);
             }
             "have lines" in {
-                "\n" + hex.edgetop + hex.top + hex.bot should be(new HexField(1, hex.col).field)
+                "\n" + hex.edgetop + hex.top(0) + hex.bot should be(new HexField(1, hex.col).field)
             }
             "be equal to a field with the size: 6 - 9" in {
                 hex.field should be(new HexField(6, 9).field)
@@ -45,10 +45,27 @@ class HexSpecTest extends AnyWordSpec {
                 hex.edgetop should be("");
             }
             "not have any lines" in {
-                hex.edgetop + hex.top + hex.bot should be(new HexField(0, 0).field)
+                hex.edgetop + hex.top(0) + hex.bot should be(new HexField(0, 0).field)
             }
             "be empty when printed completly" in {
                 hex.field should be("")
+            }
+        }
+
+        "created as Single Cell" should {
+
+            val hex = new HexField(1, 1)
+
+            "contain nothing" in {
+                hex.matrix(0)(0) should be(' ')
+            }
+            "or contain a X" in {
+                hex.placeX(0, 0)
+                hex.matrix(0)(0) should be('X')
+            }
+            "or contain a O" in {
+                hex.placeO(0, 0)
+                hex.matrix(0)(0) should be('O')
             }
         }
     }
