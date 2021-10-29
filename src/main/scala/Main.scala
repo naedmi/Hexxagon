da8ef66c46acb7c87e8fc0dc88032a38ce4124e8
Ostabo <osi.borkenhagen@gmail.com>
2021-10-29 12:22:35 +0200

    matrix bla

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,13 +1,13 @@
 package scala
 
 @main def Hex: Unit = {
-  val h = new HexField(2, 2);
-  //h.placeX(0, 0)
+  val h = new HexField(0, 0);
+  h.placeX(0, 0)
   //h.placeX(0, 1)
   print(h.field);
 }
 
-case class HexField(var lines:Int = 6, val col:Int = 9):
+case class HexField(var lines:Int = 6, var col:Int = 9):
   var condition = 1;
   if (col == 0 || lines == 0) {
     condition = 0;
@@ -15,7 +15,8 @@ case class HexField(var lines:Int = 6, val col:Int = 9):
   
   val usecol = col / 2
   if lines == 0 then lines = 1
-  val matrix = Array.ofDim[Char](lines, usecol + 1)
+  if col == 0 then col = 1
+  val matrix = Array.ofDim[Char](lines, col)
   val stone = ' ';
   if (condition != 0) {
     
@@ -46,8 +47,8 @@ case class HexField(var lines:Int = 6, val col:Int = 9):
   def bot(line:Int): String = {
     
     var res = "\\___/" * condition
-    for (x <- 0 until usecol) {
-      res += " " + matrix(line)(1).toString * condition + " \\___/" * condition
+    for (x <- 1 until col - 1 by 2) {
+      res += " " + matrix(line)(x).toString * condition + " \\___/" * condition
     }
     res + "\n" * condition
   }
@@ -55,8 +56,8 @@ case class HexField(var lines:Int = 6, val col:Int = 9):
   def top(line:Int): String = {
     
     var res = "/ " * condition + matrix(line)(0).toString * condition + " \\" * condition
-    for (x <- 0 until usecol) {
-      res += "___/ " + matrix(line)(x) + " \\" * usecol
+    for (x <- 2 until col by 2) {
+      res += "___/ " + matrix(line)(x) + " \\"
     }
     res + "\n" * condition
   }
