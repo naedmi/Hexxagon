79e59c82e5f6f442917e843392152dbcc351ffcd
nadine <namied@pm.me>
2021-11-26 13:06:57 +0100

    we love more whitespaces

--- src/main/scala/controller/GameStatus.scala
+++ src/main/scala/controller/GameStatus.scala
@@ -1,6 +1,6 @@
 package controller
 
-enum GameStatus(mes:String):
+enum GameStatus(mes: String):
   case IDLE extends GameStatus("")
   case GAMEOVER extends GameStatus("GAME OVER")
   case TURNPLAYER1 extends GameStatus("Player 1 to place X")
