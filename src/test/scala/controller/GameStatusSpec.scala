41abbaaa1d15e01ecd6706565e8efeb6725c47be
Ostabo <osi.borkenhagen@gmail.com>
2021-11-26 13:05:11 +0100

    scala 3 enum

--- src/test/scala/controller/GameStatusSpec.scala
+++ src/test/scala/controller/GameStatusSpec.scala
@@ -7,13 +7,13 @@ import org.scalatest.matchers.should.Matchers._
 class GameStatusSpec extends AnyWordSpec {
     "A GameStatus" when {
         "map descriptions for each value" in {
-            GameStatus.message(IDLE) should be ("")
-            GameStatus.message(GAMEOVER) should be ("GAME OVER")
-            GameStatus.message(TURNPLAYER1) should be ("Player 1 to place X")
-            GameStatus.message(TURNPLAYER2) should be ("Player 2 to place O")
+            IDLE.message() should be ("")
+            GAMEOVER.message() should be ("GAME OVER")
+            TURNPLAYER1.message() should be ("Player 1 to place X")
+            TURNPLAYER2.message() should be ("Player 2 to place O")
         }
         "have a type with a value of the given ENUMS" in {
-            GameStatus.values should be(Set( IDLE, GAMEOVER, TURNPLAYER1, TURNPLAYER2 ))
+            GameStatus.values should be(Array( IDLE, GAMEOVER, TURNPLAYER1, TURNPLAYER2 ))
         }
     }
 }
