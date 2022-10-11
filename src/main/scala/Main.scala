f2216c64820f2a1037dc1f80a302ab220fd985da
Ostabo <osi.borkenhagen@gmail.com>
2022-10-11 21:23:09 +0200

    wrapper for controller

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -2,33 +2,42 @@ package scala
 
 import aview.TUI
 import aview.gui.GUI
-import HexModule.{given}
-import scala.io.StdIn.readLine
-import scalafx.application.JFXApp3
-import model.fieldComponent.fieldBaseImpl._
 import controller.controllerComponent.ControllerInterface
+import controller.controllerComponent.controllerBaseImpl.Controller
+import scalafx.application.JFXApp3
+
+import scala.HexModule.given
+import scala.io.StdIn.readLine
 
 object starter {
   val tui = TUI()
+
   def runTUI: Unit = {
     println(tui.startmes)
     tui.handleInput("save")
     var input = ""
     var tmp = tui.handleInput(input)
-    while(!tmp.equals(Some("Exiting.")) & !tmp.equals(Some("Filled with X.")) & !tmp.equals(Some("Filled with O."))) {
+    while (!tmp.equals(Some("Exiting.")) & !tmp.equals(Some("Filled with X.")) & !tmp.equals(Some("Filled with O."))) {
       input = readLine()
       tmp = tui.handleInput(input)
       if !tmp.isEmpty then println(tmp.get)
-    } 
+    }
   }
+
   def runGUI = {
     GUI().start()
   }
+
+  def runController: ControllerInterface[Char] = {
+    Controller()
+  }
 }
+
 object MainTUI extends App {
-    starter.runTUI
+  starter.runTUI
 }
+
 object MainGUI extends JFXApp3 {
-    override def start() = 
-      starter.runGUI
+  override def start() =
+    starter.runGUI
 }
