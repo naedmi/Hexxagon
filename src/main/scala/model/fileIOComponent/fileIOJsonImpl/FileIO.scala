930791df01127f47eace6d56f7306f07b815b485
Ostabo <osi.borkenhagen@gmail.com>
2022-11-21 20:34:50 +0100

    remove unnecessary extra field variable

--- src/main/scala/model/fileIOComponent/fileIOJsonImpl/FileIO.scala
+++ src/main/scala/model/fileIOComponent/fileIOJsonImpl/FileIO.scala
@@ -1,8 +1,9 @@
 package model.fileIOComponent.fileIOJsonImpl
 
-import model.fileIOComponent.FileIOInterface
 import model.fieldComponent.FieldInterface
-import play.api.libs.json._
+import model.fileIOComponent.FileIOInterface
+import play.api.libs.json.*
+
 import scala.io.Source
 
 class FileIO extends FileIOInterface {
@@ -12,18 +13,18 @@ class FileIO extends FileIOInterface {
     val rows = (json \ "field" \ "rows").get.toString.toInt
     val cols = (json \ "field" \ "cols").get.toString.toInt
     var field = FlexibleModule(rows, cols).given_FieldInterface_Char
-  
+
     for (index <- 0 until rows * cols) {
-      val row = (json \\ "row")(index).as[Int]
-      val col = (json \\ "col")(index).as[Int]
-      val cell = (json \\ "cell")(index).as[String]
+      val row = (json \\ "row") (index).as[Int]
+      val col = (json \\ "col") (index).as[Int]
+      val cell = (json \\ "cell") (index).as[String]
       field = field.placeAlways(cell.head, col, row)
     }
     field
   }
 
   override def save(field: FieldInterface[Char]): Unit = {
-    import java.io._
+    import java.io.*
     val pw = new PrintWriter(new File("field.json"))
     pw.write(Json.prettyPrint(fieldToJson(field)))
     pw.close
@@ -33,17 +34,24 @@ class FileIO extends FileIOInterface {
   override def exportGame(field: FieldInterface[Char], xcount: Int, ocount: Int, turn: Int): String =
     gameToJson(field, xcount, ocount, turn).toString
 
+  def gameToJson(field: FieldInterface[Char], xcount: Int, ocount: Int, turn: Int) = {
+    Json.obj(
+      "xcount" -> JsNumber(xcount),
+      "ocount" -> JsNumber(ocount),
+      "turn" -> JsNumber(turn),
+      "field" -> fieldToJson(field)
+    )
+  }
+
   def fieldToJson(field: FieldInterface[Char]) = {
     Json.obj(
-      "field" -> Json.obj(
-        "rows" -> JsNumber(field.matrix.row),
-        "cols" -> JsNumber(field.matrix.col),
-        "cells" -> Json.toJson(
-          for {
-            row <- 0 until field.matrix.row;
-            col <- 0 until field.matrix.col
-          } yield cellToJson(field, row, col)
-        )
+      "rows" -> JsNumber(field.matrix.row),
+      "cols" -> JsNumber(field.matrix.col),
+      "cells" -> Json.toJson(
+        for {
+          row <- 0 until field.matrix.row;
+          col <- 0 until field.matrix.col
+        } yield cellToJson(field, row, col)
       )
     )
   }
@@ -53,15 +61,6 @@ class FileIO extends FileIOInterface {
       "row" -> row,
       "col" -> col,
       "cell" -> Json.toJson(field.matrix.cell(col, row).toString)
-    )
-  }
-  
-  def gameToJson(field: FieldInterface[Char], xcount: Int, ocount: Int, turn: Int) = {
-    Json.obj(
-      "xcount" -> JsNumber(xcount),
-      "ocount" -> JsNumber(ocount),
-      "turn" -> JsNumber(turn),
-      "field" -> fieldToJson(field)
     )
   }
 }
